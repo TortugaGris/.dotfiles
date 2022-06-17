61874974af1ca3848e7b556d0845b42e1daba58e
Javier Zavala <javierzavalatorres@gmail.com>
2022-06-17 18:38:14 -0400

    colorls

--- README.md
+++ README.md
@@ -1,25 +1,5 @@
-# Neovim Configuration
-Install neovim
+# Dotfiles installation
 ```
-sudo apt-get update
-sudo apt-get install neovim
-```
-
-Install vim-plug
-```
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-```
-
-Copy `init.vim` to `.config/nvim/` directory
-```
-mkdir ./config/nvim/
-cp init.vim ~/.config/nvim/
-```
-
-In `init.vim`
-```
-:source %
-:PlugInstall
+sh -s 'curl https://raw.githubusercontent.com/TortugaGris/.dotfiles/main/install.sh'
 ```
 
--- install.sh
+++ install.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 apt-get update
-
 apt-get install sudo
-
-sudo apt-get install zsh git neovim curl
+sudo apt-get install zsh git neovim
 
 cd
 
 git clone https://github.com/TortugaGris/.dotfiles.git
 
 # Neovim
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -s 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 mkdir -p ~/.config/nvim/
 ln -fs ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 nvim --headless +PlugInstall +qa
 
-# zsh
+# Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-ln -fs ~/.dotfiles/.zshrc ~/.zshrc
 ln -fs ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+
+# ZSH
+ln -fs ~/.dotfiles/.zshrc ~/.zshrc
 chsh -s "$(command -v zsh)"
 
-
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+# Colorls
+sudo apt-get install ruby-dev ubuntu-dev-tools
+gem install colorls
+echo 'source $(dirname $(gem which colorls))/tab_complete.sh' >> ~/.zshrc
+echo 'alias ls="colorls"' >> ~/.zshrc
