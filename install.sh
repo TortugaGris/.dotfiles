bbdd5d37853ecb7aa6d5a5a47c881559b0df0ffa
Javier Zavala <javierzavalatorres@gmail.com>
2022-06-17 19:20:28 -0400

    install fix

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ cd
 git clone https://github.com/TortugaGris/.dotfiles.git
 
 # Neovim
-sh -s 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/nvim/
 ln -fs ~/.dotfiles/init.vim ~/.config/nvim/init.vim
