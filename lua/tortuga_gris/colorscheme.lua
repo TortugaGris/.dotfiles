b8e383d041d75b499c00e88d0ccb40926464abe5
Javier Zavala <javierzavalatorres@gmail.com>
2024-05-07 11:38:45 -0400

    add catpuccino colorscheme

--- lua/tortuga_gris/colorscheme.lua
+++ lua/tortuga_gris/colorscheme.lua
@@ -1,13 +1,4 @@
-local colorscheme = "kanagawa"
-
-local status_ok, kanagawa = pcall(require, "kanagawa")
-if not status_ok then
-  return
-end
-
-kanagawa.setup({
-    transparent = false,
-})
+local colorscheme = "catppuccin-macchiato"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
